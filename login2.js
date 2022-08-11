86a750090163be64f3fdb8e17f73991bf1c71042
irvan-create <108305174+irvan-create@users.noreply.github.com>
2022-08-11 21:32:56 +0800

    Log Book Instrument Lab Mikrobiologi BPOM Palu

---
+++ login2.js
@@ -0,0 +1,66 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="199701022020121003" && password=="199701022020121003") // irvan
+{
+    alert("Hello Irvan, wilujeng sumping");
+    window.open("https://sites.google.com/view/mikrobiologi-balai-pom-di-palu/home", "width : 10%");
+    return false;
+    
+}
+else if(username=="197906292010121001" && password=="197906292010121001") // achid
+{
+    alert("Hello Achid, wilujeng sumping");
+    window.open("https://sites.google.com/view/mikrobiologi-balai-pom-di-palu/home","width : 67%");
+    return false;
+}
+else if(username=="197810162005022002" && password=="197810162005022002") //wuri
+{
+    alert("Hello Wuri, wilujeng sumping");
+    window.open("https://sites.google.com/view/mikrobiologi-balai-pom-di-palu/home","width : 67%");
+    return false;
+}
+else if(username=="199505152019032004" && password=="199505152019032004") //trisiana
+{
+    alert("Hello Trisiana, wilujeng sumping");
+    window.open("https://sites.google.com/view/mikrobiologi-balai-pom-di-palu/home","width : 67%");
+    return false;
+}
+else if(username=="199110292015022004" && password=="199110292015022004") // reny
+{
+    alert("Hello Reny, wilujeng sumping");
+    window.open("https://sites.google.com/view/mikrobiologi-balai-pom-di-palu/home","width : 67%");
+    return false;
+}
+else if(username=="199707042022032001" && password=="199707042022032001") // rahma
+{
+    alert("Hello Rahma, wilujeng sumping");
+    window.open("https://sites.google.com/view/mikrobiologi-balai-pom-di-palu/home","width : 67%");
+    return false;
+}
+else if(username=="Adhil" && password=="mikro123")
+{
+    alert("Hello Adhil, wilujeng sumping");
+    window.open("https://sites.google.com/view/mikrobiologi-balai-pom-di-palu/home","width : 67%");
+    return false;
+}
+else if(username=="Arifki" && password=="mikro123")
+{
+    alert("Hello Arifki, wilujeng sumping");
+    window.open("https://sites.google.com/view/mikrobiologi-balai-pom-di-palu/home","width : 67%");
+    return false;
+}
+else if(username=="Balai Kalibrasi" && password=="Balai Kalibrasi")
+{
+    alert("Hello Balai Kalibrasi, wilujeng sumping");
+    window.open("https://sites.google.com/view/mikrobiologi-balai-pom-di-palu/home","width : 67%");
+    return false;
+}
+else
+{
+    alert("login gagal");
+}
+
+
+}
